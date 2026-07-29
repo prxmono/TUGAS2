85443c6d1529b34518c84eeb02bf1c76fc66f73d
Pramono <prxmono@gmail.com>
2026-07-29 16:02:41 +0800

    project1DONE

--- project1.js
+++ project1.js
@@ -4,12 +4,13 @@ function hitungTotalBuku(bukuHarganya) {
     let totalBelanja = 0
     for (let i = 0; i < bukuHarganya.length; i++) {
         totalBelanja += bukuHarganya[i]
-    }
-    console.log(`Total Belanja : Rp.${totalBelanja}`)
-    Let harga
-    if (totalBelanja >= 150000) {
-        totalBelanja - 15 / 100
-    }
-    console.log(`Harga Akhir : ${totalBelanja}`)
+    };
+    console.log(`Total Belanja : Rp.${totalBelanja}`);
+
+    let diskon = totalBelanja * 0.15
+    if (totalBelanja >= 150000)
+        console.log("Selamat Anda Mendapatkan Diskon 15% !!!")
+        totalBelanja -= diskon
+        console.log(`Harga Diskon : Rp.${totalBelanja}`);
 }
 hitungTotalBuku(bukuHarga)
