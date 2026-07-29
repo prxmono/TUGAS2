7b639292f2d1f0defa53db5a7a65106c0df1277e
Pramono <prxmono@gmail.com>
2026-07-29 17:39:08 +0800

    project2DONE

---
+++ project2.js
@@ -0,0 +1,23 @@
+let ipkCalon = [
+    { ipk: 3.5, pendapatanortu: 5000000 },
+    { ipk: 3.5, pendapatanortu: 4000000 },
+    { ipk: 3.4, pendapatanortu: 4000000 }
+];
+
+function seleksiBeasiswa(calonIpk) {
+    let lulus = 0
+    let tidakLulus = 0
+    for (let i = 0; i < calonIpk.length; i++)
+
+        if (calonIpk[i].ipk >= 3.5 && calonIpk[i].pendapatanortu >= 5000000) {
+            console.log(`Status peserta ke ${i+1} : Lulus`)
+            lulus += 1
+        } else {
+            console.log(`Status peserta ke ${i+1} : Tidak Lulus`)
+            tidakLulus += 1
+        }
+
+    console.log(`Yang lulus : ${lulus}`);
+    console.log(`Yang tidak lulus : ${tidakLulus}`);
+}
+seleksiBeasiswa(ipkCalon);
