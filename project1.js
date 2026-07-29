f464a01bcdf53397ac6fcb2cd96cf9e3da79ad35
Pramono <prxmono@gmail.com>
2026-07-29 15:31:10 +0800

    project1

--- project1.js
+++ project1.js
@@ -1,1 +1,15 @@
-test
+let bukuHarga = [50000, 50000, 50000]
+
+function hitungTotalBuku(bukuHarganya) {
+    let totalBelanja = 0
+    for (let i = 0; i < bukuHarganya.length; i++) {
+        totalBelanja += bukuHarganya[i]
+    }
+    console.log(`Total Belanja : Rp.${totalBelanja}`)
+    Let harga
+    if (totalBelanja >= 150000) {
+        totalBelanja - 15 / 100
+    }
+    console.log(`Harga Akhir : ${totalBelanja}`)
+}
+hitungTotalBuku(bukuHarga)
